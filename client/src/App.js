325791ecb18ba9e05c37da4f695ed1f1df00bffd
Jonathan Daly <dalyj138@gmail.com>
2019-06-13 13:59:40 -0400

    Created routes in App.js, and added helpers/api.js  && helpers/auth.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, NavLink, withRouter } from 'react-router-dom';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+import Users from './components/Users';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  logout = () => {
+    localStorage.removeItem('token');
+    this.props.history.push('/signin');
+  }
+  
+  render() {
+    return (
+      <>
+        <h1>Welcome</h1>
+
+        <ul>
+          <li><NavLink to="/signin" component={Signin}>Sign In</NavLink></li>
+          <li><NavLink to="/signup" component={Signup}>Sign Up</NavLink></li>
+          <li><NavLink to="/users" component={Users}>Users</NavLink></li>
+          <li><button onClick={this.logout}>Logout</button></li>
+        </ul>
+
+        <main>
+          <Route path="/signin" component={Signin} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/users" component={Users} />
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;
